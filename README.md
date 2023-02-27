438ccc8e97914c9993e7a5a8e28932c32f491b44
gyyang <gyyang@users.noreply.github.com>
2023-02-26 20:36:19 -0500

    adding main

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # unity-starter
 Stater code for playing with Unity
+
+If you haven't done so, download Unity (https://unity.com/download)
+
+Create conda virtual environment by running in command line
+
+```conda env create -f environment.yml```
+
+Activate the virtual environment `unitystarter` by running
+
+```conda activate unitystarter```
+
+Run the sample game by running ``python main.py``
---
+++ main.py
@@ -0,0 +1,20 @@
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
+
+# Location of the Unity binary
+FILENAME = "./unity/CareBot3D/carebot3d.app"
+
+# Build env and convert to Gym env
+unity_env = UnityEnvironment(file_name=FILENAME, seed=1, side_channels=[])
+env = UnityToGymWrapper(unity_env)
+
+# Start interacting with the environment
+obs = env.reset()
+for i in range(1000):
+    action = env.action_space.sample()
+    # action = brain(obs)
+    obs, reward, done, info = env.step(action)
+    print(i)
+    if reward > 0:
+        print(obs.shape, reward, done, info)
+
