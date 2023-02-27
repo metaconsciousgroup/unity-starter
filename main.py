3afbb0365cd5a7f22e9793751e81c4c4cf5f3a9e
gyyang <gyyang@users.noreply.github.com>
2023-02-27 12:45:55 -0500

    minor update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 
@@ -12,6 +13,7 @@ env = UnityToGymWrapper(unity_env)
 obs = env.reset()
 for i in range(1000):
     action = env.action_space.sample()
+    # action = np.array([0, 0, 1, 0])
     # action = brain(obs)
     obs, reward, done, info = env.step(action)
     print(i)
